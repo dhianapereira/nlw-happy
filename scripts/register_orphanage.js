d6c14ba525dccc72b8050fdaddaa7e481315b84d
Dhiana <dhianaslv@outlook.com>
2020-10-17 15:57:09 -0300

    action to the buttons of attendance to the weekends

--- scripts/register_orphanage.js
+++ scripts/register_orphanage.js
@@ -56,3 +56,16 @@ function deleteField(event) {
 
   span.parentNode.remove();
 }
+
+function toggleSelect(event) {
+  document.querySelectorAll(".button-select button").forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  const button = event.currentTarget;
+  button.classList.add("active");
+
+  const input = document.querySelector('[name="open-on-weekends"]');
+
+  input.value = button.dataset.value;
+}
